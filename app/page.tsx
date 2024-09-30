0886208f05f6c93d936febcd75a0256a6a86ae1c
LiXenon <lxy19181100@outlook.com>
2024-09-30 18:24:29 +0300

    feat: Initialize the editor with basic functions

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import EditorPage from "./pages/EditorPage";
+import './globals.css';
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <EditorPage />
+    </>
   );
 }
